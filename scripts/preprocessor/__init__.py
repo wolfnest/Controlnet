9ab6dd9adfa9d5be4ecc321b0db8b13a857acdab
Warwolf <unknowgenesis@gmail.com>
2024-05-11 12:57:30 +0700

    init

---
+++ scripts/preprocessor/__init__.py
@@ -0,0 +1,6 @@
+from .inpaint import *
+from .lama_inpaint import *
+from .ip_adapter_auto import *
+from .normal_dsine import *
+from .model_free_preprocessors import *
+from .legacy.legacy_preprocessors import *
